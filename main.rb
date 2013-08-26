7559eb63411de304541ac78945707bbb077bb207
Ed Mills <molarexplorer@yahoo.co.uk>
2013-08-26 13:30:14 +0100

    username

--- main.rb
+++ main.rb
@@ -3,10 +3,40 @@ require 'sinatra'
 
 set :sessions, true
 
-get '/form' do
-	erb :form
+get '/' do
+	if session[:player_name]
+		#progress to game
+	else
+		redirect '/new_player'
+	end
 end
 
-post '/myaction' do
-	puts params['username']
+get '/new_player' do
+	erb :new_player
+end
+
+post '/new_player' do
+	session[:player_name] = params[:player_name]
+#progress to game
+redirect '/game'
+
+end
+
+get '/game' do
+	#set up game values
+	suits=['Hearts', 'Diamonds', 'Clubs', 'Spades']
+	values=['1','2','3','4','5','6','7','8','9','J','Q','K','A']
+	session[:deck]=suits.product(values).shuffle
+
+	session[:dealer_cards]=[]
+	session[:player_cards]=[]
+	
+		session[:dealer_cards]<<session[:deck].pop
+		session[:dealer_cards]<<session[:deck].pop
+		session[:player_cards]<<session[:deck].pop
+		session[:player_cards]<<session[:deck].pop
+	
+	#render template
+
+	erb :game
 end
